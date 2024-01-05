440244a951e782430f72a8a37dac97866aaa5ae3
Trevor <trevor@quanta-inc.com>
2024-01-04 21:11:00 -0500

    fix: support long responses

--- main.py
+++ main.py
@@ -21,12 +21,26 @@ def chat(messages):
     else:
         return False
 
+async def send_long_message(channel, message):
+    if len(message) > 2000:
+        newline_index = message[:2000].rfind("\n")
+        if newline_index == -1:
+            newline_index = message[:2000].rfind(" ")
+            if newline_index == -1:
+                newline_index = 2000
+
+        await channel.send(message[:newline_index])
+        await send_long_message(channel, message[newline_index:])
+    else:
+        await channel.send(message)
+
 @tree.command(name="ask", description=f"Ask me a question", guild=discord.Object(id=os.getenv("DISCORD_GUILD_ID")))
 async def on_ask(interaction, question: str):
     await interaction.response.defer(thinking=True)
     chat_response = chat([{"role": "user", "content": question}])
     if chat_response:
-        await interaction.followup.send(chat_response)
+        await interaction.followup.send(f"__Question: {question}__")
+        await send_long_message(interaction.channel, chat_response)
     else:
         await interaction.followup.send(f"Could not process question: {question}")
 
@@ -35,7 +49,7 @@ async def on_chat(interaction):
     thread = await interaction.channel.create_thread(name=f"Chat with {client.user.display_name}", auto_archive_duration=60, reason="Started a discord-ollama chat")
     await thread.add_user(interaction.user)
     await thread.send(f"Hello! I am {client.user.display_name}, a chatbot using the {os.getenv('OLLAMA_MODEL')} model. Use /bye to end the chat.")
-    await interaction.response.send_message("Chat started!", ephemeral=True)
+    await interaction.response.send_message(f"Chat started! <#{str(thread.id)}>", ephemeral=True)
 
 @tree.command(name="bye", description=f"End a chat", guild=discord.Object(id=os.getenv("DISCORD_GUILD_ID")))
 async def on_bye(interaction):
@@ -71,9 +85,9 @@ async def on_message(message):
         
         chat_response = chat(messages)
         if chat_response:
-            await message.channel.send(content=chat_response)
+            await send_long_message(message.channel, chat_response)
         else:
-            await message.channel.send(content=f"Could not process message: {message.content}")
+            await message.channel.send(f"Could not process message: {message.content}")
 
 client.run(os.getenv("DISCORD_TOKEN"))
 print("Discord-ollama stopped")
